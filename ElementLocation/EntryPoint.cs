c383d65b4e5770dec7397ff2a38f14d391df0d7d
Alex Moreno <aem.gamboa@gmail.com>
2018-05-09 18:14:16 -0300

    Se agrega clases 38, 39, sección 8

---
+++ ElementLocation/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+        string url = "http://testing.todvachev.com";
+
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement imagen = driver.FindElement(By.CssSelector("#page-17 > div > p:nth-child(1) > a > img"));
+
+        driver.Manage().Window.Maximize();
+
+        Console.WriteLine(imagen.Location.X);
+        Console.WriteLine(imagen.Location.Y);
+        Console.WriteLine(imagen.Size.Width);
+        Console.WriteLine(imagen.Size.Height);
+
+    }
+}
---
+++ ExecuteJavaScriptCSharp/EntryPoint.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class Program
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+        string url = "http://testing.todvachev.com/";
+
+        driver.Navigate().GoToUrl(url);
+
+        //Texto del sitio
+        IWebElement content = driver.FindElement(By.CssSelector("#page-17 > div"));
+
+        //Imagen del sitio
+        //IWebElement image = driver.FindElement(By.CssSelector("#page-17 > div > p:nth-child(1) > a > img"));
+
+
+
+        //Desaparece imagen
+        //string script = "arguments[0].style[\"display\"]=\"none\"";
+
+        //Cambia de color el texto del sitio
+        //string script = "arguments[0].style[\"color\"] = \"green\"";
+
+        //Cambia el color del borde y lo deja con grosor
+        //string script = "arguments[0].style[\"border\"] = \"2px solid green\"";
+
+        SetStyle(driver, content, "color", "yellow");
+
+    }
+
+
+    static void SetStyle(IWebDriver driver, IWebElement element, string style, string styleValue)
+    {
+        string script = string.Format("arguments[0].style[\"{0}\"] = \"{1}\"", style, styleValue);
+        IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+
+        jsExecutor.ExecuteScript(script, element);
+    }
+}
