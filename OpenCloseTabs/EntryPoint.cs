0fcb62bb719f787092af576cb8f59cf34b61f2c7
Alex Moreno <aem.gamboa@gmail.com>
2018-05-11 18:26:09 -0300

    Sección 10, Clase 48

--- OpenCloseTabs/EntryPoint.cs
+++ OpenCloseTabs/EntryPoint.cs
@@ -1,10 +1,38 @@
 ﻿using OpenQA.Selenium;
-namespace OpenCloseTabs
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System.Collections.Generic;
+using System.Linq;
+
+class EntryPoint
 {
-    class EntryPoint
+    static void Main()
     {
-        static void Main(string[] args)
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://google.com");
+
+        Actions actions = new Actions(driver);
+
+        actions.SendKeys(Keys.Control + "t").Build().Perform();
+
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+
+        List<string> handles = driver.WindowHandles.ToList();
+
+        driver.SwitchTo().Window(handles[1]);
+        driver.Navigate().GoToUrl("http://gmail.com");
+
+        for(int i = 0; i< handles.Count; i++)
         {
+            if(i != 2)
+            {
+                driver.SwitchTo().Window(handles[i]);
+                driver.Close();
+            }
         }
+
     }
+
 }
---
+++ PhantomProject/EntryPoint.cs
@@ -0,0 +1,75 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> extractedLinks = new List<string>();
+        List<string> extractedTitle = new List<string>();
+        List<string> extractedContent = new List<string>();
+
+        IWebDriver driver = new PhantomJSDriver();
+        IWebElement titleElement;
+        IWebElement contentElement;
+
+        string[] pageSource;
+
+        string sitemapURL = "http://testing.todvachev.com/sitemap-posttype-post.xml";
+        string titleSelector = "#main-content > article > header > h1";
+        string contentSelector = "#main-content > article > div";
+
+        int startIndex = 0;
+        int linkLength = 0;
+
+        driver.Navigate().GoToUrl(sitemapURL);
+
+        pageSource = driver.PageSource.Split(' ');
+
+        foreach(var item in pageSource)
+        {
+            if (item.Contains("<loc>"))
+            {
+                startIndex = item.IndexOf("<loc>") + 5;
+                linkLength = item.IndexOf("</loc>") - startIndex;
+
+
+                //extractedLinks.Add(item.Substring(item.IndexOf("<loc>")));
+                extractedLinks.Add(item.Substring(startIndex, linkLength));
+                Console.WriteLine(item.Substring(startIndex, linkLength));
+
+                //Console.WriteLine(item.Substring(item.IndexOf("<loc>")));
+            }
+        }
+
+        foreach (var item in extractedLinks)
+        {
+            driver.Navigate().GoToUrl(item);
+
+            titleElement = driver.FindElement(By.CssSelector(titleSelector));
+            contentElement = driver.FindElement(By.CssSelector(contentSelector));
+
+
+            extractedTitle.Add(titleElement.Text);
+            extractedContent.Add(contentElement.Text);
+
+            //Despliega todo el valor del contenido del sitio
+            /*Console.WriteLine(titleElement.Text);
+            Console.WriteLine(contentElement.Text);*/
+        }
+
+        Directory.CreateDirectory(@"\ExtractedContent");
+
+        using (StreamWriter sw = File.CreateText(Directory.GetCurrentDirectory() + @"\ExtractedContent\ExtractedTest.txt"))
+        {
+            sw.WriteLine("TITLE: {0}",extractedTitle[0]);
+            sw.WriteLine("CONTENT:");
+            sw.Write(extractedContent[0]);
+        }
+
+        //Console.WriteLine(driver.PageSource);
+    }
+}
